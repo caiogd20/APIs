b0a53a1a61892df25a32093c0b698870b9c687c8
Caio Gonçalves Dias <caiogd20@gmail.com>
2025-08-21 09:12:04 -0300

    Canvas

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
-let caixa = document.getElementById("caixa");
-let zona1 = document.getElementById("zona1");
-let zona2 = document.getElementById("zona2");
-caixa.ondragstart = (e) => {
-  e.dataTransfer.setData("text/plain", e.target.id);
-  caixa.classList.add("black");
-}
-caixa.ondragend = (e) => {
-  caixa.classList.remove("black");
-}
-for(const z of document.querySelectorAll(".zona")) {
-  z.ondragover = (e) => {
-    e.preventDefault();
-  }
-  z.ondrop = (e) => {
-    e.preventDefault();
-    let id = e.dataTransfer.getData("text/plain");
-    let el = document.getElementById(id);
-    if (z.id === "zona1") {
-      zona1.appendChild(el);
-    } else if (z.id === "zona2") {
-      zona2.appendChild(el);
-    } else if (z.id === "zona3") {
-      zona3.appendChild(el);
-    } else if (z.id === "zona4") {
-      zona4.appendChild(el);
-    }
-  }
+let desenho = document.getElementById('desenho');
+if (desenho.getContext) {
+  let contexto = desenho.getContext('2d');
+
+  // Desenhar um retângulo azul
+  contexto.fillStyle = 'blue';
+  contexto.fillRect(10, 10, 170, 100);
+
+  contexto.fillStyle = 'blue';
+  contexto.fillRect(230, 10, 170, 100);
+
+  // Desenhar um círculo vermelho
+  contexto.beginPath();
+  contexto.arc(200, 60, 50, 0, Math.PI * 2, true);
+  contexto.fillStyle = 'red';
+  contexto.fill();
+  
+
+  contexto.beginPath();
+  contexto.arc(10, 60, 50, 0, Math.PI * 2, true);
+  contexto.fillStyle = 'red';
+  contexto.fill();
+
+  
+  
+
 }
